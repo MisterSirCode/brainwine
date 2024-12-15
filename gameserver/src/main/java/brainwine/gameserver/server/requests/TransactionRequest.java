0e8e16fb4a8b548add802bdf7e335ed251bc081e
kuroppoi <68156848+kuroppoi@users.noreply.github.com>
2024-12-15 02:05:22 +0100

    Add thirst

--- gameserver/src/main/java/brainwine/gameserver/server/requests/TransactionRequest.java
+++ gameserver/src/main/java/brainwine/gameserver/server/requests/TransactionRequest.java
@@ -4,6 +4,7 @@ import brainwine.gameserver.player.Player;
 import brainwine.gameserver.server.PlayerRequest;
 import brainwine.gameserver.server.RequestInfo;
 import brainwine.gameserver.server.messages.StatMessage;
+import brainwine.gameserver.server.models.PlayerStat;
 
 @RequestInfo(id = 41)
 public class TransactionRequest extends PlayerRequest {
@@ -13,6 +14,6 @@ public class TransactionRequest extends PlayerRequest {
     @Override
     public void process(Player player) {
         player.notify("Sorry, the crown store has not been implemented yet.");
-        player.sendMessage(new StatMessage("crowns", player.getCrowns()));
+        player.sendMessage(new StatMessage(PlayerStat.CROWNS, player.getCrowns()));
     }
 }
